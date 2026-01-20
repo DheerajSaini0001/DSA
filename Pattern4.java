ee962178bae7da450071e06a51efb90da3097417
DheerajSaini0001 <dheerajsaini131652@gmail.com>
2026-01-20 14:50:07 +0530

    Done pattern 4

--- Pattern4.java
+++ Pattern4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Pattern3 {
+public class Pattern4 {
     public static void main(String[] args) {
 System.out.println("Enter the number of stars");
 Scanner sc =new Scanner(System.in);
@@ -8,13 +8,15 @@ int n =sc.nextInt();
         int i,j;
         for (i=0;i<n;i++)
         {
-            for(int s=0;s<=(n-i-1);s++){
+            for (int s=0;s<=i;s++){
                 System.err.print(" ");
             }
-            for(j=0;j<=i;j++){
+            
+            for(j=5;j>i;j--){
                 
                 System.err.print("*");
             }
+            
             System.err.println();
         }
         
